fbb678ec0df6fc0ea1f5c2f05b1657a4320c4686
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2025-09-16 20:32:35 +0530

    A7 done

---
+++ 16sept/A6/task.py
@@ -0,0 +1,18 @@
+# animals_sound = {"dog": "bark",
+#                  "cat": "meow",
+#                  "cow": "moo",
+#                  "duck": "quack",
+#                  "lion": "roar"
+#                  }
+# for animals, sounds in animals_sound.items():
+#     print(f"{animals} says {sounds}")
+
+
+# fruits = {"apple": 5, "banana": 3}
+# print("Original dictionary:", fruits)
+
+# fruits["orange"] = 8
+# print("After adding orange:", fruits)
+
+# fruits["apple"] = 10
+# print("After updating apple:", fruits)
