eda19eb1c77da45f834565d7e9aa2e2d685e4ac8
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2025-09-24 13:49:27 +0530

    L1

---
+++ 24sept/L1/task.py
@@ -0,0 +1,18 @@
+# 2 SUM
+# class Solution:
+#     def twoSum(self, nums: list[int], target: int) -> list[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i]+nums[j] == target:
+#                     return [i, j]
+
+
+# print(Solution().twoSum([0, 1, 3, 6], 9))
+
+# PALINDROME
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            return str[x] == str[::-1]
