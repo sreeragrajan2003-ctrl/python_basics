91cd45b2472bf86fef34f4b5bfb2a211ebe04b31
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2025-09-26 09:45:46 +0530

    L3 completed

---
+++ 26sept/L3.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                last = stack.pop()
+                if char == ')' and last != '(':
+                    return False
+                if char == '}' and last != '{':
+                    return False
+                if char == ']' and last != '[':
+                    return False
+        return len(stack) == 0
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+
+        dummy = ListNode()
+        current = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+
+        return dummy.next
