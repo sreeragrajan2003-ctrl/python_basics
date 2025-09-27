a6060669d03a6244aaecae9dc07a83904d6c70ad
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2025-09-27 08:12:16 +0530

    L4

---
+++ 27sept/L4.py
@@ -0,0 +1,21 @@
+# remove duplicates of list
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+
+
+# remove element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
