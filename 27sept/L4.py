0c585d6f75a665de92742e4c36801812d4e8bcdf
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2025-09-27 08:23:02 +0530

    L4 updated

--- 27sept/L4.py
+++ 27sept/L4.py
@@ -19,3 +19,13 @@ class Solution:
                 nums[k] = nums[i]
                 k += 1
         return k
+
+# check word
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
